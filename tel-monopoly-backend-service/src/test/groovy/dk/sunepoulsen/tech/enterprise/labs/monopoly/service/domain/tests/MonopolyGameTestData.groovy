8f933c818a36e8a36b97252add49a4f8d1d85ce3
sunepoulsen <sunepoulsen@gmail.com>
2020-09-26 02:14:20 +0200

    TEL-29: Removed unused test classes.

--- tel-monopoly-backend-service/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/monopoly/service/domain/tests/MonopolyGameTestData.groovy
+++
@@ -1,7 +0,0 @@
-package dk.sunepoulsen.tech.enterprise.labs.monopoly.service.domain.tests
-
-import dk.sunepoulsen.tech.enterprise.labs.monopoly.service.domain.game.MonopolyBoard
-import dk.sunepoulsen.tech.enterprise.labs.monopoly.service.domain.game.fields.AbstractMonopolyField
-
-class MonopolyGameTestData {
-}
